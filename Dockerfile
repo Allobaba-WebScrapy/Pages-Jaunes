38a83b58bc45b9f78c51d3ead80a9ff6fb3427ba
kamado <abdonsila2004@gmail.com>
2024-03-07 17:41:51 +0100

    Add next page template

---
+++ Dockerfile
@@ -0,0 +1,99 @@
+# SeleniumBase Docker Image
+FROM ubuntu:18.04
+
+#=======================================
+# Install Python and Basic Python Tools
+#=======================================
+RUN apt-get -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false update
+RUN apt-get install -y python3 python3-pip python3-setuptools python3-dev python-distribute
+RUN alias python=python3
+RUN echo "alias python=python3" >> ~/.bashrc
+
+#=================================
+# Install Bash Command Line Tools
+#=================================
+RUN apt-get -qy --no-install-recommends install \
+    sudo \
+    unzip \
+    wget \
+    curl \
+    libxi6 \
+    libgconf-2-4 \
+    vim \
+    xvfb \
+  && rm -rf /var/lib/apt/lists/*
+
+#================
+# Install Chrome
+#================
+RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list && \
+    apt-get -yqq update && \
+    apt-get -yqq install google-chrome-stable && \
+    rm -rf /var/lib/apt/lists/*
+
+
+#===========================
+# Configure Virtual Display
+#===========================
+RUN set -e
+RUN echo "Starting X virtual framebuffer (Xvfb) in background..."
+RUN Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
+RUN export DISPLAY=:99
+RUN exec "$@"
+
+#=======================
+# Update Python Version
+#=======================
+RUN apt-get update -y
+RUN apt-get -qy --no-install-recommends install python3.8
+RUN rm /usr/bin/python3
+RUN ln -s python3.8 /usr/bin/python3
+
+#=============================================
+# Allow Special Characters in Python Programs
+#=============================================
+RUN export PYTHONIOENCODING=utf8
+RUN echo "export PYTHONIOENCODING=utf8" >> ~/.bashrc
+
+#=====================
+# Set up SeleniumBase
+#=====================
+COPY sbase /SeleniumBase/sbase/
+COPY seleniumbase /SeleniumBase/seleniumbase/
+COPY examples /SeleniumBase/examples/
+COPY integrations /SeleniumBase/integrations/
+COPY requirements.txt /SeleniumBase/requirements.txt
+COPY setup.py /SeleniumBase/setup.py
+RUN find . -name '*.pyc' -delete
+RUN find . -name __pycache__ -delete
+RUN pip3 install --upgrade pip setuptools wheel
+RUN cd /SeleniumBase && ls && pip3 install -r requirements.txt --upgrade
+RUN cd /SeleniumBase && pip3 install .
+
+#=====================
+# Download WebDrivers
+#=====================
+RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux64.tar.gz
+RUN tar -xvzf geckodriver-v0.34.0-linux64.tar.gz
+RUN chmod +x geckodriver
+RUN mv geckodriver /usr/local/bin/
+RUN wget https://chromedriver.storage.googleapis.com/72.0.3626.69/chromedriver_linux64.zip
+RUN unzip chromedriver_linux64.zip
+RUN chmod +x chromedriver
+RUN mv chromedriver /usr/local/bin/
+
+#==========================================
+# Create entrypoint and grab example tests
+#==========================================
+COPY integrations/docker/docker-entrypoint.sh /
+COPY integrations/docker/run_docker_test_in_firefox.sh /
+COPY integrations/docker/run_docker_test_in_chrome.sh /
+RUN chmod +x *.sh
+COPY integrations/docker/docker_config.cfg /SeleniumBase/examples/
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+
+COPY ./index.py .
+
+CMD ["/bin/bash && python3", "index.py"]
--- index.py
+++ index.py
@@ -1,181 +1,224 @@
 from seleniumbase import SB
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 import json
 import base64
-from seleniumbase import BaseCase
-import time
 
-def decode_link(attribute_link) :
-    print("[1] :",attribute_link)
-    #?---------------// Decode URL \\---------------
-    # Parse the JSON string
-    data_dict = json.loads(attribute_link)
-    print("[2] :",data_dict)
-    # Extract the URL and decode it
-    encoded_url = data_dict['url']
-    print("[3] :",encoded_url)
-    decoded_url = base64.b64decode(encoded_url).decode('utf-8')
-    print("[4] :",decoded_url)
-    #? ---------------------------------------------
-    return {"attr":attribute_link,"encoded_url":encoded_url, "decoded_url":decoded_url}
+
+chrome_options = {
+    # "args": ["--headless"]
+    "args": ["--disable-gpu"]
+}
+
 
 def verify_success(sb):
     sb.sleep(1)
 
-def Handle_switch_link(sb,url) :
-    print("\n",url)
-    sb.open(url)
 
-    #!---------------------------- Title ----------------------------
-    if sb.is_element_visible('div.header-main-infos h1.noTrad'):
-        title = sb.get_text('div.header-main-infos h1.noTrad').splitlines()[0]
-        print("Title: ",title)
-    #!---------------------------- Activite ----------------------------
-    if sb.is_element_visible('span.activite'):
-        activite = sb.get_text('span.activite')
-        print("Activite: ",activite)
-    #!---------------------------- Rank ----------------------------
-    if sb.is_element_visible('span.note_moyenne'):
-        rank = sb.get_text('span.note_moyenne')
-        print("Rank: ",rank)
-    #!---------------------------- Avis ----------------------------
-    if sb.is_element_visible('span.bi-rating'):
-        avis = sb.get_text('span.bi-rating')
-        print("Avis: ",avis)
-    #!---------------------------- Adress ----------------------------
-    if sb.is_element_visible('div.address-container span.noTrad'):
-        adress = sb.get_text('div.address-container span.noTrad')
-        print("Adress: ",adress)
-    if sb.is_element_visible('div.lvs-container span.value'):
-        website = sb.get_text('div.lvs-container span.value')
-        print("Website: ",website)
-    #!---------------------------- Horaires ----------------------------
-    if sb.is_element_visible('ul.liste-horaires-principaux'):
-        horaires_elements = sb.find_elements('li.horaire-ouvert')
-        list_horaires = []
-        for index, horaire_element in enumerate(horaires_elements) :
-            if sb.is_element_visible(f'li.horaire-ouvert:nth-child({index+1})'):
-                day = sb.get_text(f'li.horaire-ouvert:nth-child({index+1}) p.jour')
-                horaire = sb.get_text(f'li.horaire-ouvert:nth-child({index+1}) p.liste')
-                list_horaires.append({"jour":day,"horaire":horaire})
-        print("Horaires: ",list_horaires)
-    #!---------------------------- Réseaux Sociaux ----------------------------
-    if sb.is_element_visible('div.bloc-info-sites-reseaux ul.clearfix'):
-        links_elements = sb.find_elements('ul.clearfix li.premiere-visibilite')
-        list_links = []
-
-        for index, link_element in enumerate(links_elements) :
-            if sb.is_element_visible(f'ul.clearfix li.premiere-visibilite:nth-child({index+1}) a.pj-link') :
-                href_link = sb.get_attribute(f'ul.clearfix li.premiere-visibilite:nth-child({index+1}) a.pj-link', 'href')
-                if(href_link!="#") :
-                    list_links.append(href_link)
-                else :
-                    attribute_link = sb.get_attribute(f'ul.clearfix li.premiere-visibilite:nth-child({index+1}) a.pj-link', 'data-pjlb')
-                    list_links.append(decode_link(attribute_link))
-        print("Réseaux Sociaux: ",list_links)
-
-    if sb.is_element_visible('div.zone-fantomas a.hidden-phone'):
-        # sb.click("a.hidden-phone")
-        print("Phone Container success")
-
-        # r = requests.get(url) 
-        # soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
-        # print(soup.prettify()) 
-        # print(sb.get_page_source())
-        # table = soup.find('div', attrs = {'id':'all_quotes'})
-
-
-    if sb.is_element_visible('div.num-container'):
-        print("Phone success")
-        phone = sb.get_text('div.num-container span.coord-numero-inscription')
-        print("Phone: ",phone)
-
-
-
-def passCloudFareVerification() :
-        try:
-            verify_success(sb)
-        except Exception:
-            if sb.is_element_visible('input[value*="Verify"]'):
-                sb.click('input[value*="Verify"]')
-            elif sb.is_element_visible('iframe[title*="challenge"]'):
-                sb.switch_to_frame('iframe[title*="challenge"]')
-                sb.click('span.mark')
-                return True
-            else:
-                passCloudFareVerification()
-        try:
-            verify_success(sb)
-        except Exception:
-            passCloudFareVerification()
-    
-    
-
-
-with SB(uc_cdp=True, guest_mode=True,headless=False) as sb:
-    sb.open('https://www.pagesjaunes.fr/annuaire/paris-75/restaurants')
-    # sb.open('https://webcache.googleusercontent.com/search?q=cache:https://www.pagesjaunes.fr/annuaire/paris-75/restaurants')
-# 
-    # Handle Verification
-
-    passCloudFareVerification()
-    
+def passCloudFareVerification(sb):
     try:
         verify_success(sb)
     except Exception:
-        if sb.is_element_visible('input[value*="Verify"]',timeout=10):
+        if sb.is_element_visible('input[value*="Verify"]'):
             sb.click('input[value*="Verify"]')
-        elif sb.is_element_visible('iframe[title*="challenge"]',timeout=10):
+        elif sb.is_element_visible('iframe[title*="challenge"]'):
             sb.switch_to_frame('iframe[title*="challenge"]')
-            sb.click('span.mark')
+            sb.click("span.mark")
+            return True
         else:
-            # passCloudFareVerification()
-            raise Exception('Detected!')
+            passCloudFareVerification(sb)
+    try:
+        verify_success(sb)
+    except Exception:
+        passCloudFareVerification(sb)
+
+
+def make_perfect_url(base_url):
+    # Parse the base URL
+    parsed_url = urlparse(base_url)
+
+    # Extract city and category information from the path
+    path_parts = parsed_url.path.split("/")
+    city = path_parts[-2] if len(path_parts) >= 3 else None
+    category = path_parts[-1] if len(path_parts) >= 2 else None
+
+    # Construct query parameters
+    query_params = {
+        "quoiqui": [category] if category else ["restaurants"],
+        "ou": [city] if city else ["paris-75"],
+        "page": ["1"],
+    }
+
+    # Reconstruct the URL with perfect structure
+    perfect_url = urlunparse(
+        (
+            parsed_url.scheme,
+            parsed_url.netloc,
+            "/annuaire/chercherlespros",
+            parsed_url.params,
+            urlencode(query_params, doseq=True),
+            parsed_url.fragment,
+        )
+    )
+    return perfect_url
+
+
+def decode_link(attribute_link):
+    # ?---------------// Decode URL \\---------------
+    # Parse the JSON string
+    data_dict = json.loads(attribute_link)
+    # Extract the URL and decode it
+    encoded_url = data_dict["url"]
+    decoded_url = base64.b64decode(encoded_url).decode("utf-8")
+    # ? ---------------------------------------------
+    return decoded_url
+
+
+def handle_card_info(sb, card_url):
+    print("\n", card_url)
+    sb.open(card_url["base_url"])
+
+    #!---------------------------- Title ----------------------------
+    if sb.is_element_visible("div.header-main-infos h1.noTrad"):
+        title = sb.get_text("div.header-main-infos h1.noTrad").splitlines()[
+            0
+        ]  # => get just the title
+        print("Title: ", title)
+
+    #!---------------------------- Activite ----------------------------
+    if sb.is_element_visible("span.activite"):
+        activite = sb.get_text("span.activite")
+        print("Activite: ", activite)
+
+    #!---------------------------- Rank ----------------------------
+    if sb.is_element_visible("span.note_moyenne"):
+        rank = sb.get_text("span.note_moyenne")
+        print("Rank: ", rank)
+
+    #!---------------------------- Avis ----------------------------
+    if sb.is_element_visible("span.bi-rating"):
+        avis = sb.get_text("span.bi-rating")
+        print("Avis: ", avis)
+
+    #!---------------------------- Adress ----------------------------
+    if sb.is_element_visible("div.address-container span.noTrad"):
+        adress = sb.get_text("div.address-container span.noTrad")
+        print("Adress: ", adress)
+    if sb.is_element_visible("div.lvs-container span.value"):
+        website = sb.get_text("div.lvs-container span.value")
+        print("Website: ", website)
+
+    #!---------------------------- Horaires ----------------------------
+    if sb.is_element_visible("ul.liste-horaires-principaux"):
+        horaires_elements = sb.find_elements("li.horaire-ouvert")
+        list_horaires = []
+        for index, horaire_element in enumerate(horaires_elements):
+            if sb.is_element_visible(f"li.horaire-ouvert:nth-child({index+1})"):
+                day = sb.get_text(f"li.horaire-ouvert:nth-child({index+1}) p.jour")
+                horaire = sb.get_text(f"li.horaire-ouvert:nth-child({index+1}) p.liste")
+                list_horaires.append({"jour": day, "horaire": horaire})
+        print("Horaires: ", list_horaires)
+
+    #!---------------------------- Réseaux Sociaux ----------------------------
+    if sb.is_element_visible("div.bloc-info-sites-reseaux ul.clearfix"):
+        links_elements = sb.find_elements("ul.clearfix li.premiere-visibilite")
+        list_links = []
+
+        for index, link_element in enumerate(links_elements):
+            if sb.is_element_visible(
+                f"ul.clearfix li.premiere-visibilite:nth-child({index+1}) a.pj-link"
+            ):
+                href_link = sb.get_attribute(
+                    f"ul.clearfix li.premiere-visibilite:nth-child({index+1}) a.pj-link",
+                    "href",
+                )
+                if (
+                    href_link
+                    == f"https://www.pagesjaunes.fr/pros/{card_url['card_id']}#"
+                ):
+                    attribute_link = sb.get_attribute(
+                        f"ul.clearfix li.premiere-visibilite:nth-child({index+1}) a.pj-link",
+                        "data-pjlb",
+                    )
+                    list_links.append(decode_link(attribute_link))
+                else:
+                    list_links.append(href_link)
+        print("Réseaux Sociaux: ", list_links)
+
+    #!---------------------------- Telephone ----------------------------
+    if sb.is_element_visible('a[title="Appeler ce professionnel"]'):
+        # if sb.is_element_visible('span.coord-numero-mobile'):
+        phones_number = sb.find_elements('a[title="Appeler ce professionnel"]')
+        for phone_number in phones_number:
+            attribute = phone_number.get_attribute("data-pjlb")
+            print(decode_link(attribute))
+
+
+# --------------------------------------- Start App ----------------------------------------
+with SB(uc_cdp=True, guest_mode=True, headless=False, undetected=True) as sb:
+    sb.set_window_size(600, 1200)
+    sb.open("https://www.pagesjaunes.fr")
+
+    # Handle CloudFare Verification
+    try:
+        verify_success(sb)
+    except Exception:
+        if sb.is_element_visible('input[value*="Verify"]', timeout=10):
+            sb.click('input[value*="Verify"]')
+        elif sb.is_element_visible('iframe[title*="challenge"]', timeout=10):
+            sb.switch_to_frame('iframe[title*="challenge"]')
+            sb.click("span.mark")
+        else:
+            # sb.open('https://www.pagesjaunes.fr')
+            raise Exception("Detected!")
         try:
             verify_success(sb)
         except Exception:
-            raise Exception('Detected!')
-        
+            # sb.open('https://www.pagesjaunes.fr')
+            raise Exception("Detected!")
+
     # Handle Coockies
-    if sb.is_element_visible('span.didomi-continue-without-agreeing'):
-            print('Done Cookie')
-            #? <span class='didomi-continue-without-agreeing'>
-            sb.click('span.didomi-continue-without-agreeing')
+    if sb.is_element_visible("span.didomi-continue-without-agreeing"):
+        print("Done Cookie")
+        # ? <span class='didomi-continue-without-agreeing'>
+        sb.click("span.didomi-continue-without-agreeing")
 
-    # Get all card
-    if sb.is_element_visible('ul.bi-list'):
-        print('Done <ul> List\n')    #? <span class='didomi-continue-without-agreeing'>
-        lists_li = sb.find_elements('ul.bi-list li.bi-generic')
-        # lists = sb.find_elements('ul.bi-list li.bi-generic div.bi-header-title h3')
-        lists_url = []
-        for index, list in enumerate(lists_li):
-            list_id = sb.get_attribute(f'li.bi-generic:nth-child({index+1})','id').split('-')[1]
-            lists_url.append(f'https://www.pagesjaunes.fr/pros/{list_id}#zoneHoraires')
-        for url in lists_url[:1] :
-            Handle_switch_link(sb, url)
+    def app(base_url):
+        sb.open(base_url)
+        # https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=restaurants&ou=paris-75&page=1
+        # sb.open('https://webcache.googleusercontent.com/search?q=cache:https://www.pagesjaunes.fr/annuaire/paris-75/restaurants')
 
+        # Handle Coockies
+        if sb.is_element_visible("span.didomi-continue-without-agreeing"):
+            print("Done Cookie")
+            # ? <span class='didomi-continue-without-agreeing'>
+            sb.click("span.didomi-continue-without-agreeing")
+        else:
+            pass
 
+        # Get all card 1
+        if sb.is_element_visible("ul.bi-list"):
+            print(
+                "Done <ul> List\n"
+            )  # ? <span class='didomi-continue-without-agreeing'>
+            # lists_li = sb.find_elements('ul.bi-list li.bi')
+            # lists_card_url = []
+            # for index, list in enumerate(lists_li):
+            #     list_id = sb.get_attribute(f'li.bi:nth-child({index+1})','id').split('-')[1]
+            #     lists_card_url.append({"base_url":f'https://www.pagesjaunes.fr/pros/{list_id}#zoneHoraires', "card_id":list_id})
+            # print(lists_card_url)
+            # for card_url in lists_card_url[:1] :
+            #     handle_card_info(sb, card_url)
 
-
-    # time.sleep(100)
-    # Scraping data example:
-    #! Let's say we want to scrape the names of restaurants from the page.
-    # restaurant_names = sb.find_elements('span.denomination-links')
-
-    # # Iterate through the elements and print their text (restaurant names)
-    # for name in restaurant_names:
-    #     print(name.text)
-
-
-
-
-
-# BaseCase.main(__name__, __file__)
-
-class MyTestClass(BaseCase):
-    def test_swag_labs(self):
-        self.open('https://www.pagesjaunes.fr/annuaire/paris-75/restaurants')
-        time.sleep(12)
-        print(self.get_page_source())
-        
-
-
+    base_urls = [
+        "https://www.pagesjaunes.fr/annuaire/paris-75/coiffeurs",
+        "https://www.pagesjaunes.fr/annuaire/paris-75/restaurants",
+        "https://www.pagesjaunes.fr/annuaire/paris-75/boulangeries-patisseries-artisans",
+        "https://www.pagesjaunes.fr/annuaire/paris-75/agences-immobilieres",
+        "https://www.pagesjaunes.fr/annuaire/paris-75/clubs-de-sport-divers",
+    ]
+    for base_url in base_urls:
+        try:
+            app(make_perfect_url(base_url))
+        except:
+            print("Failed")
+        print("|_" * 70)
