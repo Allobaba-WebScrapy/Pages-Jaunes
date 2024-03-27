82de327d0b85ed2ba9758d6c363e36e97f9cb32a
ilorez <zobirnajdaoui@gmail.com>
2024-03-27 12:33:28 +0000

    remove unused packages and update file  format

--- index.py
+++ index.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify, request, Response
 from flask_cors import CORS  # Import CORS from flask_cors module
 from scrape import PageJaunesScraper
-from data import client_urls
 import json
-import time
-
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes in the Flask app
@@ -13,7 +10,7 @@ scraper = PageJaunesScraper()
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"Flask": "Welcome to the PageJaunes Scraper API!"})
+    return jsonify({"Flask": "Welcome to " "the PageJaunes Scraper API!"})
 
 
 @app.route("/setup", methods=["POST"])
@@ -29,56 +26,23 @@ def setup():
 def stream():
     def event_stream():
         results = list()
-        yield f"event: progress\ndata: {json.dumps({"type":"progress", "message": "Scraping started!"})}\n\n"
+        yield f"event: progress\ndata: {json.dumps({'type':'progress', 'message': 'Scraping started!'})}\n\n"
         for result in scraper.run():
-            if "type" in result and result["type"]=="progress":
+            if "type" in result and result["type"] == "progress":
                 yield f"event: progress\ndata: {json.dumps(result)}\n\n"
-            elif "type" in result and result["type"]=='error':
+            elif "type" in result and result["type"] == "error":
                 yield f"errorEvent: error\ndata: {json.dumps(result)}\n\n"
             else:
                 results.append(result)
                 yield f"data: {json.dumps(result)}\n\n"
         if not results:
-            yield f"event: errorEvent\ndata: {json.dumps({"type":"error", "message":"Bybass verification failed No result!"})}\n\n"
+            yield f"event: errorEvent\ndata: {json.dumps({'type':'error', 'message':'Bybass verification failed No result!'})}\n\n"
             return
         else:
             # save_to_csv(result, f"static/{scraper.fileName}.csv")
             yield f"event: done\ndata: done\n\n"
 
     return Response(event_stream(), mimetype="text/event-stream")
-
-    
-#! Server Side Events (SSE) route For Test
-@app.route("/s")
-def index():
-    # params = request.get_json()
-
-    def generate():
-        # Start time
-        start_time = time.time()
-        print("Server is running!")
-
-        scraper.server_urls = client_urls[:1]
-
-        results = list()
-        for result in scraper.run():
-            if "type" in result and result["type"]=="progress":
-                yield f"event: progress\ndata: {json.dumps(result)}\n\n"
-            elif "type" in result and result["type"]=="error":
-                yield f"errorEvent: error\ndata: {json.dumps(result)}\n\n"
-            else:
-                yield f"data: {json.dumps(result)}\n\n"
-        if not results:
-            yield "event: errorEvent\ndata: Verification failed Not result\n\n"
-            return
-        else:
-            yield f"event: done\ndata: done\n\n"
-
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print("Execution time:", execution_time, "seconds")
-
-    return Response(generate(), mimetype="text/event-stream")
 
 
 if __name__ == "__main__":
