324a70c20c90d1b99241544bfcf577b06777e100
kamado <abdonsila2004@gmail.com>
2024-04-05 14:11:59 +0000

    omment start app

--- index.py
+++ index.py
@@ -42,5 +42,5 @@ def stream():
     return Response(event_stream(), mimetype="text/event-stream")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4080)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=4080)
